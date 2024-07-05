da2267cb5e76b1b88e2fd7e1749f545f2f2d5443
Kristian Bouw <kbouw@hey.com>
2024-07-05 16:47:15 -0400

    created breadcrumbs component for functions and nodes

---
+++ ThirdParty/Web/src/components/ui/FunctionBreadcrumbs.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from '../ui/breadcrumb';
+import { useSelectedClass } from '../../providers/SelectedClassContextProvider';
+
+const NodeBreadcrumbs: React.FC = () => {
+    const { selectedClass, selectedFunction } = useSelectedClass();
+
+    return (
+        <Breadcrumb className="mb-8">
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Classes</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                {selectedClass && (
+                    <>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href={`/class/${selectedClass.name}`}>{selectedClass.name}</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                    </>
+                )}
+                {selectedFunction && (
+                    <>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>{selectedFunction.name}</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </>
+                )}
+            </BreadcrumbList>
+        </Breadcrumb>
+    );
+};
+
+export default NodeBreadcrumbs;
