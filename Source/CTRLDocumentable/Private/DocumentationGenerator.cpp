b103f3b1137097ed1383c425f76ae02464aa40fb
Kristian Bouw <kbouw@hey.com>
2025-03-12 12:45:01 -0400

    updated copyright year

--- Source/CTRLDocumentable/Private/DocumentationGenerator.cpp
+++ Source/CTRLDocumentable/Private/DocumentationGenerator.cpp
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Copyright (C) 2023-2024 NTY.studio. All Rights Reserved.
+// Copyright (C) 2023-2025 NTY.studio. All Rights Reserved.
 
 #include "DocumentationGenerator.h"
 #include "CTRLDocumentableLog.h"
