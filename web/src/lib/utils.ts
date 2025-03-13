ac20f07da69c80baec7df529047273167dd6f46c
Kristian Bouw <kbouw@hey.com>
2025-03-12 22:58:49 -0400

    created a util function to reset selected class and navigate to root

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { TreeItemConfig } from "../types/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleHomeClick = (e: React.MouseEvent, setSelectedClass: (selectedClass: TreeItemConfig | null) => void, navigate: (path: string) => void) => {
+  e.preventDefault();
+  setSelectedClass(null);
+  navigate('/');
+};
