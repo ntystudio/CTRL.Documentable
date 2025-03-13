fb52d99bfb89a40b700f200b1b6faf7ecc66576b
Kristian Bouw <kbouw@hey.com>
2025-03-12 21:24:01 -0400

    refactored MainLayout to use the new sidebar component and provider

--- web/src/components/layout/MainLayout.tsx
+++ web/src/components/layout/MainLayout.tsx
@@ -1,19 +1,41 @@
-import {PropsWithChildren} from 'react';
-import Sidebar from './Sidebar';
-import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "../../components/ui/resizable";
+import { PropsWithChildren } from 'react';
+import { Separator } from "../ui/separator";
+import { AppSidebar } from '../app-sidebar';
+import { SidebarProvider, SidebarTrigger, SidebarInset } from '../ui/sidebar';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '../ui/breadcrumb';
+import { ThemeSwitcher } from 'src/components/ui/ThemeSwitcher';
 
-const MainLayout = ({children}: PropsWithChildren) => {
+const MainLayout = ({ children }: PropsWithChildren) => {
     return (
         <main className="flex w-full h-screen overflow-hidden">
-            <ResizablePanelGroup direction="horizontal" className="w-full">
-                <ResizablePanel defaultSize={20} minSize={16} maxSize={25}>
-                    <Sidebar/>
-                </ResizablePanel>
-                <ResizableHandle withHandle/>
-                <ResizablePanel defaultSize={80}>
-                    <div className="h-full overflow-y-auto px-4">{children}</div>
-                </ResizablePanel>
-            </ResizablePanelGroup>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b justify-between">
+                        <div className="flex items-center gap-2 px-3">
+                            <SidebarTrigger />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="#">
+                                            Building Your Application
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                        <ThemeSwitcher />
+                    </header>
+                    <div className="flex-1 overflow-y-auto">
+                        {children}
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
         </main>
     );
 };
