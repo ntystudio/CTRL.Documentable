e0ba32d45b9e7c291b7a065aec758a208af19e5f
Kristian Bouw <kbouw@hey.com>
2025-03-13 00:24:58 -0400

    refactored the FunctionList layout and design

--- web/src/components/class/FunctionList.tsx
+++ web/src/components/class/FunctionList.tsx
@@ -1,29 +1,27 @@
-import React, {FC, useState, useEffect, useCallback} from 'react';
+import {FC, useState, useEffect, useCallback} from 'react';
 import { FunctionConfig } from '../../types/types';
-import {
-    AlertDialog, AlertDialogAction, AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger
-} from '../ui/alert-dialog';
 import { Separator } from '../ui/separator';
 import { Input } from '../ui/input';
 import {Button} from '../ui/button';
-import {Badge} from '../ui/badge';
 import {DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuCheckboxItem } from '../ui/dropdown-menu';
-import nodes from '../../data/nodes.json'; // Import the JSON data
+import nodes from '../../data/nodes.json';
 import { useNavigate } from 'react-router-dom';
 import { useSelectedClass } from '../../providers/SelectedClassContextProvider';
-import { LinkIcon } from '../ui/icons/LinkIcon';
 import {Alert, AlertDescription, AlertTitle} from '../ui/alert';
-import {BookmarkFilledIcon, ExclamationTriangleIcon, Pencil1Icon, TrashIcon} from '@radix-ui/react-icons';
+import {ExclamationTriangleIcon, Pencil1Icon, TrashIcon} from '@radix-ui/react-icons';
 import {useNotes} from "../../providers/NotesContextProvider";
 import {NoteDialog} from "../../components/NoteDialog";
 import {Card, CardContent} from "../../components/ui/card";
 import {Popover, PopoverContent, PopoverTrigger} from "../../components/ui/popover";
+import { X } from 'lucide-react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from '../ui/table';
 
 type FunctionSearchFields = {
     name: boolean;
@@ -117,7 +115,7 @@ export const FunctionList: FC<FunctionListProps> = ({ functions }) => {
 
     return (
         <>
-            <div className="flex flex-row w-full max-w-[700px] mb-12">
+            {/* <div className="flex flex-row w-full max-w-[700px] mb-12">
                 <div className="relative flex-grow mr-4">
                     <Input
                         type="text"
@@ -132,12 +130,7 @@ export const FunctionList: FC<FunctionListProps> = ({ functions }) => {
                             className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                             aria-label="Clear search"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                                 fill="currentColor">
-                                <path fillRule="evenodd"
-                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                      clipRule="evenodd"/>
-                            </svg>
+                            <X className="size-4" />
                         </button>
                     )}
                 </div>
@@ -178,7 +171,7 @@ export const FunctionList: FC<FunctionListProps> = ({ functions }) => {
                         </DropdownMenuCheckboxItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-            </div>
+            </div> */}
             {filteredFunctions.length === 0 ? (
                 <Alert className="max-w-[400px]">
                     <ExclamationTriangleIcon className="h-6 w-6"/>
@@ -188,134 +181,80 @@ export const FunctionList: FC<FunctionListProps> = ({ functions }) => {
                     </AlertDescription>
                 </Alert>
             ) : (
-                filteredFunctions.map((func, index) => (
-                    <React.Fragment key={index}>
-                        <Separator className="my-8"/>
-                        <div className="grid grid-cols-12 gap-8 w-full max-w-[1200px]">
-                            <div className="col-span-4 flex flex-col">
-                                <Card>
-                                    <CardContent>
-                                        <div className="text-lg font-bold text-informational mt-4">{func.returnType}</div>
-                                        {func.flags && func.flags.length > 0 && (
-                                            <>
-                                                <Separator className="my-2"/>
-                                                <Popover>
-                                                    <PopoverTrigger asChild>
-                                                        <Button variant="outline" className="mt-2 w-full">Metadata</Button>
-                                                    </PopoverTrigger>
-                                                    <PopoverContent className="w-auto" align="start">
-                                                        <div className="flex flex-col items-start">
-                                                            {[
-                                                                ...func.flags
-                                                                    ?.filter(flag => !flag.startsWith('ModuleRelativePath') && !flag.startsWith('ToolTip') && !flag.startsWith('Comment')),
-                                                                ...func.flags
-                                                                    ?.filter(flag => flag.startsWith('ModuleRelativePath')),
-                                                            ].map((flag, index, array) => (
-                                                                <div key={index} className="font-mono text-sm">
-                                                                    {flag}
-                                                                    {index !== array.length - 1 && <Separator className="my-2"/>}
-                                                                </div>
-                                                            ))}
-                                                        </div>
-                                                    </PopoverContent>
-                                                </Popover>
-                                            </>
+                <div className="w-full">
+                    <Table className="w-full max-w-[1100px]">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[50%]">Function</TableHead>
+                                <TableHead className="w-[40%]">Parameters</TableHead>
+                                <TableHead className="w-[10%]">Metadata</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {filteredFunctions.map((func, index) => (
+                                <TableRow key={index}>
+                                    <TableCell className="align-top">
+                                        <div>
+                                            <div className="flex items-center gap-2 text-lg">
+                                                <span className="text-informational font-medium font-mono">{func.returnType}</span>
+                                                <span className="font-medium max-w-[500px] truncate">{func.name}</span>
+                                            </div>
+                                            {func.description && (
+                                                <p className="text-muted-foreground text-sm mt-1">{func.description}</p>
+                                            )}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell className="align-top">
+                                        {func.parameters?.length === 0 ? (
+                                            <span className="text-muted-foreground text-sm">No parameters</span>
+                                        ) : (
+                                            <div className="space-y-1">
+                                                {func.parameters?.map((param, idx) => (
+                                                    <div key={idx} className="text-sm">
+                                                        <span className="text-informational font-mono mr-2">{param.type}</span>
+                                                        {' '}
+                                                        <span>{param.name}</span>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         )}
-                                    </CardContent>
-                                </Card>
-                            </div>
-                            <div className="col-span-8 flex flex-col">
-                                <h2 className="text-2xl font-bold pb-3">
-                                    <button onClick={() => selectFunctionHandler(func)}
-                                            className="flex flex-row items-center hover:underline">
-                                        <LinkIcon />
-                                        <span className="ml-2">{func.name}</span>
-                                    </button>
-                                </h2>
-
-                                <div className=" ml-[26px]">
-
-                                    {func.description && (
-                                        <p className="text-muted-foreground text-lg mb-4">{func.description}</p>
-                                    )}
-
-                                    {hasNote(selectedClass.name, func.name) ? (
-                                        <div
-                                            className="p-4 rounded-md mb-4 relative group border-2 bg-[#e8ebff] border-[#1fa2fd] dark:border-[#ffc229] dark:bg-[#1b1614]">
-                                            <div className="flex items-start">
-                                                <div className="flex-shrink-0 mr-2">
-                                                    <BookmarkFilledIcon className="w-5 h-5 text-[#1fa2fd] dark:text-[#ffc229]"/>
-                                                </div>
-                                                <p className="flex-grow">{getNoteContent(selectedClass.name, func.name)}</p>
-                                            </div>
-                                            <div
-                                                className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                                                <Button
-                                                    onClick={() => handleAddOrEditNote(func)}
-                                                    className="py-1 px-1.5 mr-1"
-                                                    size={'sm'}
-                                                    aria-label="Edit note"
-                                                >
-                                                    <Pencil1Icon className="h-5 w-5"/>
-                                                </Button>
-                                                <AlertDialog>
-                                                    <AlertDialogTrigger asChild>
-                                                        <Button
-                                                            className="py-1 px-1.5"
-                                                            size={'sm'}
-                                                            variant="destructive"
-                                                            aria-label="Delete note"
-                                                        >
-                                                            <TrashIcon className="h-5 w-5"/>
-                                                        </Button>
-                                                    </AlertDialogTrigger>
-                                                    <AlertDialogContent>
-                                                        <AlertDialogHeader>
-                                                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                                                            <AlertDialogDescription>
-                                                                This action cannot be undone. This will permanently delete
-                                                                the note for {func.name}.
-                                                            </AlertDialogDescription>
-                                                        </AlertDialogHeader>
-                                                        <AlertDialogFooter>
-                                                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                                            <AlertDialogAction onClick={() => handleDeleteNote(func)}
-                                                                               variant="destructive">
-                                                                Delete
-                                                            </AlertDialogAction>
-                                                        </AlertDialogFooter>
-                                                    </AlertDialogContent>
-                                                </AlertDialog>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        <div className="mt-2">
-                                            <Button size={'sm'} onClick={() => handleAddOrEditNote(func)}>
-                                                Add Note
-                                            </Button>
-                                        </div>
-                                    )}
-
-                                    <Separator className="my-6 max-w-xl"/>
-
-                                    <div>
-                                        <p className="uppercase text-sm font-semibold mb-3">Parameters</p>
-                                        {func.parameters?.length === 0
-                                            ? <p className="text-muted-foreground">No parameters provided</p>
-                                            : func.parameters?.map((param, index) => (
-                                                <div key={index} className="flex mb-2">
-                                                    <Badge className="rounded-lg border border-border bg-card dark:border-border dark:bg-card py-0.5 px-1.5 text-black dark:text-white font-mono text-base">{param.name}:</Badge>
-                                                    <span className="font-bold ml-2 text-informational text-lg">{param.type}</span>
-                                                </div>
-                                            ))
-                                        }
-                                    </div>
-                                </div>
-
-                            </div>
-                    </div>
-                </React.Fragment>
-            )))}
+                                    </TableCell>
+                                    <TableCell className="text-right align-top">
+                                        {func.flags && func.flags.length > 0 && (
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <Button 
+                                                        variant="outline" 
+                                                        size="sm"
+                                                        onClick={(e) => e.stopPropagation()}
+                                                    >
+                                                        View Metadata
+                                                    </Button>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto" align="end">
+                                                    <div className="flex flex-col items-start">
+                                                        {[
+                                                            ...func.flags
+                                                                ?.filter(flag => !flag.startsWith('ModuleRelativePath') && !flag.startsWith('ToolTip') && !flag.startsWith('Comment')),
+                                                            ...func.flags
+                                                                ?.filter(flag => flag.startsWith('ModuleRelativePath')),
+                                                        ].map((flag, index, array) => (
+                                                            <div key={index} className="font-mono text-sm">
+                                                                {flag}
+                                                                {index !== array.length - 1 && <Separator className="my-2"/>}
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                </PopoverContent>
+                                            </Popover>
+                                        )}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
             <NoteDialog
                 isOpen={isNoteDialogOpen}
                 onClose={() => setIsNoteDialogOpen(false)}
